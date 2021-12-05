d6f531d4506b8071a5a7bfb30e87fc05625c5824
Na9asaki <roma.siyamenko.100@mail.ru>
2021-12-05 19:37:08 +0300

    For Heroku

--- src/main/java/startcoder/Run.java
+++ src/main/java/startcoder/Run.java
@@ -1,9 +1,8 @@
 package startcoder;
 
-import java.io.IOException;
 
 public class Run {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         new Bot().serve();
     }
 }
